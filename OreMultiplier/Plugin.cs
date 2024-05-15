96b3c7d68dfec1d363f7190912bb836a670737f8
erkle64 <erkle64@gmail.com>
2024-05-15 14:35:56 +1200

    Added ore vein support.  Added overrides for reservoirs and veins.

--- OreMultiplier/Plugin.cs
+++ OreMultiplier/Plugin.cs
@@ -1,5 +1,6 @@
 using C3.ModKit;
 using HarmonyLib;
+using System.Linq;
 using Unfoundry;
 using UnityEngine;
 
@@ -12,12 +13,16 @@ namespace OreMultiplier
             MODNAME = "OreMultiplier",
             AUTHOR = "erkle64",
             GUID = AUTHOR + "." + MODNAME,
-            VERSION = "0.2.3";
+            VERSION = "0.2.4";
 
         public static LogSource log;
 
         public static TypedConfigEntry<float> chanceMultiplier;
         public static TypedConfigEntry<float> yieldMultiplier;
+        public static TypedConfigEntry<float> reservoirChanceMultiplierOverride;
+        public static TypedConfigEntry<float> reservoirYieldMultiplierOverride;
+        public static TypedConfigEntry<float> veinChanceMultiplierOverride;
+        public static TypedConfigEntry<float> veinYieldMultiplierOverride;
 
         public Plugin()
         {
@@ -25,8 +30,32 @@ namespace OreMultiplier
 
             new Config(GUID)
                 .Group("Ore Multiplication")
-                    .Entry(out chanceMultiplier, "chanceMultiplier", 2, true, "Ore patch chance multiplication factor.")
-                    .Entry(out yieldMultiplier, "yieldMultiplier", 8, true, "Ore patch yield multiplication factor.")
+                    .Entry(out chanceMultiplier, "chanceMultiplier", 2.0f, true,
+                        "Ore patch chance multiplication factor.",
+                        "Increase this to spawn more ore patches, reservoirs and veins.")
+                    .Entry(out yieldMultiplier, "yieldMultiplier", 8.0f, true,
+                        "Ore patch yield multiplication factor.",
+                        "Increase this to make ore patches, reservoirs and veins contain more ore.")
+                .EndGroup()
+                .Group("Reservoir Overrides")
+                    .Entry(out reservoirChanceMultiplierOverride, "reservoirChanceMultiplierOverride", 0.0f, true,
+                        "Override chance multiplier for olumite reservoirs.",
+                        "0 = use chanceMultiplier.",
+                        "1 = disable reservoir chance multiplication.")
+                    .Entry(out reservoirYieldMultiplierOverride, "reservoirYieldMultiplierOverride", 0.0f, true,
+                        "Override yield multiplier for olumite reservoirs.",
+                        "0 = use yieldMultiplier.",
+                        "1 = disable reservoir yield multiplication.")
+                .EndGroup()
+                .Group("Vein Overrides")
+                    .Entry(out veinChanceMultiplierOverride, "veinChanceMultiplierOverride", 0.0f, true,
+                        "Override chance multiplier for ore veins.",
+                        "0 = use chanceMultiplier.",
+                        "1 = disable ore vein chance multiplication.")
+                    .Entry(out veinYieldMultiplierOverride, "veinYieldMultiplierOverride", 0.0f, true,
+                        "Override yield multiplier for ore veins.",
+                        "0 = use yieldMultiplier.",
+                        "1 = disable ore vein yield multiplication.")
                 .EndGroup()
                 .Load()
                 .Save();
@@ -44,7 +73,7 @@ namespace OreMultiplier
             [HarmonyPrefix]
             public static void TerrainBlockType_onLoad(TerrainBlockType __instance)
             {
-                if (__instance.oreSpawn_chancePerChunk_ground > 0)
+                if (__instance.flags.HasFlagNonAlloc(TerrainBlockType.TerrainTypeFlags.Ore))
                 {
                     var chanceFactor = chanceMultiplier.Get();
                     if (chanceFactor > 0.0f && chanceFactor != 1.0f)
@@ -61,6 +90,16 @@ namespace OreMultiplier
                         __instance.averageYield = (int)Mathf.Ceil(__instance.averageYield * yieldFactor);
                     }
                 }
+                else if (__instance.flags.HasFlagNonAlloc(TerrainBlockType.TerrainTypeFlags.OreVeinMineable))
+                {
+                    var yieldFactor = yieldMultiplier.Get();
+                    if (veinYieldMultiplierOverride.Get() > 0.0f) yieldFactor = veinYieldMultiplierOverride.Get();
+                    if (yieldFactor > 0.0f && yieldFactor != 1.0f)
+                    {
+                        log.Log($"Multiplying {__instance.identifier} yield x{yieldFactor}");
+                        __instance.oreVeinMineable_averageYield = (int)Mathf.Ceil(__instance.oreVeinMineable_averageYield * yieldFactor);
+                    }
+                }
             }
 
             [HarmonyPatch(typeof(ReservoirTemplate), nameof(ReservoirTemplate.onLoad))]
@@ -70,6 +109,7 @@ namespace OreMultiplier
                 if (__instance.chancePerChunk_ground > 0)
                 {
                     var chanceFactor = chanceMultiplier.Get();
+                    if (reservoirChanceMultiplierOverride.Get() > 0.0f) chanceFactor = reservoirChanceMultiplierOverride.Get();
                     if (chanceFactor > 0.0f && chanceFactor != 1.0f)
                     {
                         log.Log($"Multiplying {__instance.identifier} chance x{chanceFactor}");
@@ -78,10 +118,27 @@ namespace OreMultiplier
                     }
 
                     var yieldFactor = yieldMultiplier.Get();
+                    if (reservoirYieldMultiplierOverride.Get() > 0.0f) yieldFactor = reservoirYieldMultiplierOverride.Get();
                     if (yieldFactor > 0.0f && yieldFactor != 1.0f)
                     {
                         log.Log($"Multiplying {__instance.identifier} yield x{yieldFactor}");
                         __instance.contentIncreasePerTile_str = (System.Convert.ToSingle(__instance.contentIncreasePerTile_str) * yieldFactor).ToString(System.Globalization.CultureInfo.InvariantCulture);
+                    }
+                }
+            }
+
+            [HarmonyPatch(typeof(OreVeinTemplate), nameof(OreVeinTemplate.onLoad))]
+            [HarmonyPrefix]
+            public static void OreVeinTemplate_onLoad(OreVeinTemplate __instance)
+            {
+                if (__instance.spawnChancePerOreChunk > 0)
+                {
+                    var chanceFactor = chanceMultiplier.Get();
+                    if (veinChanceMultiplierOverride.Get() > 0.0f) chanceFactor = veinChanceMultiplierOverride.Get();
+                    if (chanceFactor > 0.0f && chanceFactor != 1.0f)
+                    {
+                        log.Log($"Multiplying {__instance.identifier} chance x{chanceFactor}");
+                        __instance.spawnChancePerOreChunk = (uint)Mathf.Ceil(__instance.spawnChancePerOreChunk / (float)chanceFactor);
                     }
                 }
             }
